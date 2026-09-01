6a9709d83c80deba5ff458c32191e9c57c0c9807
Maitri Rana <maitri.rana15@gmail.com>
2026-09-01 11:41:32 -0600

    New changes

--- Backend/CRUD.WebAPI/Middleware/ExceptionMiddleware.cs
+++ Backend/CRUD.WebAPI/Middleware/ExceptionMiddleware.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 namespace CRUD.WebAPI.Middleware
 {
     //Checking for unhandled exceptions and logging them, returning a generic error response to the client.
+    //New changes
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
